a0d83c8e38b3c44a73a87dcfab7de54677e15da1
afloresh1 <afloresh@unjbg.edu.pe>
2024-07-15 00:07:26 -0500

    En el siguiente programa se podra analizar si el numero ingresado del mes coincide con algun dato del mes que se ingresó

--- registros4.cpp
+++ registros4.cpp
@@ -12,6 +12,7 @@ struct informacion{
 int main(){
 	informacion informacion_atletas[80];
 	int n;
+	string paiss;
 	cout<<"Ingrese la cantidad de atletas olimpicos: ";
 	cin>>n;
 	for (int i=1; i<=n; i++){
@@ -24,6 +25,22 @@ int main(){
 		cout<<"Ingrese el numero de medallas del atleta #"<<i<<" : ";
 		cin>>informacion_atletas[i].numeromedallas;
 	}
+	cout<<"Ingrese el pais que quiera: ";
+	cin>>paiss;
+	for(int k=1; k<=n; k++){
+	if(informacion_atletas[k].pais==paiss){
+		cout<<"DATOS DE LA PERSONA QUE COINCIDE DEL PAIS QUE INGRESO"<<endl;
+		cout<<k<<" Nombre: "<<informacion_atletas[k].nombre<<endl;
+		cout<<k<<" Pais de origen : "<<informacion_atletas[k].pais<<endl;
+		cout<<k<<" Disciplina : "<<informacion_atletas[k].disciplina<<endl;
+		cout<<k<<" Numero de medallas : "<<informacion_atletas[k].numeromedallas<<endl;
+	}
+	else {
+		cout<<"No coincide con ninguno "<<endl;
+	}
+	}
+	
+	
 	
 	cout<<" --------------DATOS----------------"<<endl;
 	for (int j=1; j<=n; j++){
