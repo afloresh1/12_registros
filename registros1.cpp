395bcdad577438cd90aa4adaddd95c9cd2f22daf
afloresh1 <afloresh@unjbg.edu.pe>
2024-07-14 20:09:39 -0500

    Se corrigio para el mayor sueldo y se agregó el menor sueldo

--- registros1.cpp
+++ registros1.cpp
@@ -5,12 +5,12 @@ using namespace std;
 struct empleado{
 	string nombre;
 	string sexo;
-	float sueldo;
+	float salario;
 };
 int main(){
 	empleado emp[100];
-	int n, nEm=0, k;
-	int s=0, my=0;
+	int n,nem=0, nEm=0;
+	int s=0, my=0, mn=0;
 	cout<<"Ingrese el numero de trabajadores: ";
 	cin>>n;
 	for (int i=1; i<=n; i++){
@@ -19,36 +19,43 @@ int main(){
 		getline(cin, emp[i].nombre);
 		cout<<"Ingrese el sexo del empleado "<<i<<" : ";
 		cin>>emp[i].sexo;
-		cout<<"Ingrese el sueldo del empleado "<<i<<" : ";
-		cin>>emp[i].sueldo;	
+		cout<<"Ingrese el salario del empleado "<<i<<" : ";
+		cin>>emp[i].salario;	
 		
 	}
 	
 	for (int j=1; j<=n; j++){
-		s = emp[j].sueldo;
+		s = emp[j].salario;
 		if (s>my){
 			my = s;
-			k++;
+			nem = j;
+		}	
+	}
+		cout<<" "<<endl;
+		cout<<"----------DATOS DEL EMPLEADO CON MAYOR SALARIO --------"<<endl;
+		cout<<" "<<endl;
+		cout<<"El mayor salario lo tiene el empleado "<<nem<<endl;
+		cout<<"Nombre del empleado: "<<emp[nem].nombre<<endl;
+		cout<<"Sexo del empleado: "<<emp[nem].sexo<<endl;
+		cout<<"Salario del empleado: "<<emp[nem].salario<<endl;
+		cout<<" "<<endl;
+	
+	mn= my;
+	s=0;
+	for (int k=1; k<=n; k++){
+	s = emp[k].salario;
+	if (mn>s){
+		mn = s;
+		nEm = k;
 		}	
 	}
 	cout<<" "<<endl;
-	cout<<"----------DATOS DEL EMPLEADO CON MAYOR SUELDO --------"<<endl;
+	cout<<"----------DATOS DEL EMPLEADO CON MENOR SALARIO --------"<<endl;
 	cout<<" "<<endl;
-	cout<<"El mayor sueldo lo tiene el empleado "<<k<<endl;
-	cout<<"Nombre del empleado: "<<emp[k].nombre<<endl;
-	cout<<"Sexo del empleado: "<<emp[k].sexo<<endl;
-	cout<<"Sueldo del empleado: "<<emp[k].sueldo<<endl;
+	cout<<"El menor salario lo tiene el empleado "<<nEm<<endl;
+	cout<<"Nombre del empleado: "<<emp[nEm].nombre<<endl;
+	cout<<"Sexo del empleado: "<<emp[nEm].sexo<<endl;
+	cout<<"Salario del empleado: "<<emp[nEm].salario<<endl;
 	cout<<" "<<endl;
-	
-	
-	cout<<"DATOS DE LOS EMPLEADOS: "<<endl;
-	for (int j=1; j<=n; j++){
-		cout<<"Nombre del empleado "<<j<<" : "<<emp[j].nombre<<endl;
-		
-		cout<<"Sexo del empleado "<<j<<" : "<<emp[j].sexo<<endl;
-		
-		cout<<"Sueldo del empleado "<<j<<" : "<<emp[j].sueldo<<endl;
-	}
-
-
 }
+
